d6c70f23ae85aa0fa37a8a6ab756c86bd32eb7dd
rcdploo <45781126+rcdploo@users.noreply.github.com>
2026-05-31 14:47:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def on_disconnect():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+    socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading', engineio_logger=False)
+
 
